edadc2829221d5197ec85714af7f2be3e9e78820
songbinliu <apple.songbin@gmail.com>
2017-06-12 17:40:22 -0400

    the script to run it

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+./updatekube -h
+#./kubeclient --kubeconfig ./admin.kubeconfig --masterUrl https://enmaster.octurbo.org:8443
+#./kubeclient --kubeconfig ./configs/en119.kubeconfig.yaml
+
+options="--kubeConfig ./configs/aws.kubeconfig.yaml "
+options=" $options --nameSpace default "
+options=" $options --scheduler-name xyzscheduler "
+options=" $options --alsologtostderr "
+
+
+./updatekube  $options
